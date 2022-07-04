e003e8e0ef25edbc1cb0301011ca80e92cc32570
Dzmitry_Kashlach <Bear doesn't like honey>
2022-07-04 14:41:37 +0300

    IsBadVersion

---
+++ src/main/java/knapsack/IsBadVersion.java
@@ -0,0 +1,44 @@
+package knapsack;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+@Setter
+@Getter
+public class IsBadVersion {
+    public int bad = 0;
+    public int firstBadVersion(int n) {
+        int[] sortedSearchArray = IntStream.range(1, n).toArray();
+        Arrays.stream(sortedSearchArray).forEach(i -> System.out.println(+i));
+        int low = 1;
+        while (low < n) {
+            int mid = low + Math.round((n - low) / 2);
+            boolean isMidBad = isBadVersion(mid,this.bad);
+            if (isMidBad) {
+                if (!isBadVersion(mid - 1,this.bad)) {
+                    return mid;
+                } else {
+                    n = mid;
+                }
+            } else {
+                low = mid;
+            }
+        }
+        return -1;
+    }
+
+    public static boolean isBadVersion(int n,int bad) {
+        return n >= bad;
+    }
+
+    public static void main(String[] args) {
+        IsBadVersion isBadVersion = new IsBadVersion();
+        isBadVersion.setBad(23);
+        int res = isBadVersion.firstBadVersion(97);
+        System.out.println("==========================================");
+        System.out.println(res);
+    }
+}
