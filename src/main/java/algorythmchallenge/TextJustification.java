3d900a45d13699e59177b02cb19c9b56f547461e
Dzmitry_Kashlach <Bear doesn't like honey>
2022-07-15 12:30:49 +0300

    TextJustification:python implementation

--- src/main/java/algorythmchallenge/TextJustification.java
+++ src/main/java/algorythmchallenge/TextJustification.java
@@ -1,9 +1,6 @@
 package algorythmchallenge;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 /*
 Sample output:
@@ -12,33 +9,35 @@ Sample output:
  */
 public class TextJustification {
 
-    public List<String>[] process(String[] input, int length) {
-        List<String>[] output = new ArrayList[input.length];
+    public List<String>[] process(String[] words, int maxLength) {
+        List<String>[] output = new ArrayList[words.length];
         int charCounter = 0;
         int lineCounter = 0;
-        List<String> outputLine = null;
-        for (String s : input) {
-            if (charCounter == 0) {
-                outputLine = new ArrayList<>();
-                outputLine.add(s);
-                charCounter = +s.length();
-                continue;
-            }
-            if (charCounter < length && charCounter > 0) {
-                outputLine.add(" " + s);
-                charCounter = charCounter + s.length();
-                if(charCounter > length){
-                    output[lineCounter] = outputLine;
-                    lineCounter++;
-                    outputLine = new ArrayList<>();
+        int curLength = 0;
+        List<String> wordLine = new ArrayList<>();
+        for (String word : words) {
+            curLength = charCounter + 1 + word.length();
+//     Case 1 exceed max length and ...
+            if (charCounter > maxLength) {
+                int spaceLeft = maxLength - charCounter;
+                if (wordLine.size() == 1) {
+                    wordLine.add(String.join("*", Collections.nCopies(maxLength - wordLine.get(0).length(), "*")));
+                }else{
+//                    calculate reminder to assign more spaces to empty slots on the left
+                      int quatient = spaceLeft/(wordLine.size()-1);
+                      int reminder = spaceLeft%(wordLine.size()-1);
+                      int frontSpaces = 0;
+                      int endSpaces = 0;
                 }
-            } else {
-                output[lineCounter] = outputLine;
-                lineCounter++;
-                outputLine.add(s);
-                charCounter = s.length();
-                continue;
             }
+//            Case 1a Single word in the line. We justify left(add whitespaces to the right)
+//            Case 1b Multiple words per line. We need to distribute whitespaces between all the words.
+//            Calculate reminder to assign more spaces to empty slots on the left
+//            Reset current line and counts;
+//     Case 2
+//            Case 2a
+//            Case 2b
+//            Last line if there are still words to be added;
         }
         return output;
     }
@@ -46,7 +45,7 @@ public class TextJustification {
     public static void main(String[] args) {
         int lineLength = 24;
         String[] input = new String[]{"Here", "is", "one", "more", "option", "to", "achieve", "excellent", "result",
-        "with","perfect","but","yet","unpredictable","high","quality"};
+                "with", "perfect", "but", "yet", "unpredictable", "high", "quality"};
         TextJustification textJustification = new TextJustification();
         List<String>[] output = textJustification.process(input, lineLength);
         try {
